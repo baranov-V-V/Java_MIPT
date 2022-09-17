496cd4b501f42898d43195fef7635f9c52303726
baranov-V-V <baranov.vv@phystech.edu>
2022-09-17 14:18:24 +0300

    added homework3

---
+++ homework3/src/test/java/mipt/baranov/AppTest.java
@@ -0,0 +1,11 @@
+package mipt.baranov;
+
+import org.junit.jupiter.api.Test;
+import org.assertj.core.api.Assertions;
+
+public class AppTest {
+    @Test
+    void helloTest() {
+        Assertions.assertThat(2 + 2).isEqualTo(4);
+    }
+}
