2004669736fe4168ee3de5d8de7a7dc0abdf7397
baranov-V-V <baranov.vv@phystech.edu>
2022-09-25 12:21:52 +0300

    added sql file execution

--- homework3/src/main/java/mipt/baranov/JDBS/DBUtil.java
+++ homework3/src/main/java/mipt/baranov/JDBS/DBUtil.java
@@ -1,18 +1,34 @@
 package mipt.baranov.JDBS;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.sql.SQLException;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class DBUtil {
     /**
-     *
-     * @param template
+     * Loads all lines from csv file into table with same name as file
+     * @param jdbc
      * @param path_to_table
      */
-    public static void loadTable(JdbcTemplate template, Path path_to_table) {
+    public static void loadCSVTable(JdbcTemplate jdbc, Path path_to_table) {
 
     }
 
-    public static void executeSqlFile(JdbcTemplate template, Path path_to_file) {
-
+    /**
+     * executes sql file
+     * @param jdbc
+     * @param path_to_file
+     */
+    public static void executeSqlFile(JdbcTemplate jdbc, Path path_to_file) throws IOException, SQLException {
+        try (Stream<String> lines = Files.lines(path_to_file, StandardCharsets.UTF_8)) {
+            final String sql_commands = lines.collect(Collectors.joining("\n"));
+            jdbc.executeStatement(statement -> {statement.execute(sql_commands);} );
+        }
     }
 }
